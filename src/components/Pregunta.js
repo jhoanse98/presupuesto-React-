9fae42b18614db15eb564ed0b73bcdb84ffa221c
jhoanse98 <mansleobenitez@gmail.com>
2020-05-18 17:57:01 -0500

    Función para definir el presupuesto

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,15 +1,41 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Pregunta = () => {
+
+    // SE DEFINE LOS VALORES DE PRESUPUESTO
+
+    const [cantidad, guardacantidad] = useState(0)
+
+    //función que establece el presupuesto
+    const definePresupuesto = e => {
+        //console.log(e.target.value) //si el número aparece de color negro en el console del navegador, significa que lo esta leyendo como un string
+        //console.log(parseInt(e.target.value)) //ahora el número aparece de color azul, por lo tanto es leído como un número
+        guardacantidad(parseInt(e.target.value))
+    
+    }
+
+    //función para agregar el presupuesto
+
+    const agregarPresupuesto = e => {
+        e.preventDefault();
+
+        //Validar
+
+        //si se pasa la validación 
+    }
+
     return (
         <Fragment>
             <h2>Coloca tu presupuesto</h2>
 
-            <form>
+            <form
+                onSubmit={agregarPresupuesto}
+            >
                 <input
                     type="number"
                     className="u-full-width"
                     placeholder="Coloca tu presupuesto"
+                    onChange={definePresupuesto}
                 />
                 <input
                     type="submit"
